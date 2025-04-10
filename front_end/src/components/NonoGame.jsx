3dffc433f17ee74208a3ebd093163a3e03a4c453
rkemmey <rachel.kemmey@gmail.com>
2025-04-10 14:36:25 -0500

    nono game component

--- front_end/src/components/NonoGame.jsx
+++ front_end/src/components/NonoGame.jsx
@@ -1,23 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { api } from "../utilities";
-//import SudGrid from "./SudGrid";
+import NonoGrid from "./NonoGrid";
 
 
 function NonoGame() {
-    const [puzzle, setPuzzle] = useState(null);
     const [solution, setSolution] = useState(null);
+    const [rowhint, setRowhint] = useState(null);
+    const [colhint, setColhint] = useState(null);
 
     const test_connection = async () => {
-        let response = await api.get("nonogram/pixels/1/");
+        let response = await api.get("nonogram/puzzle/1/");
         let data = response.data
         console.log(data);
-
+    
         try {
-            let puzzleArr = data.newboard.grids[0] // TODO: possibly store difficulty
-            setPuzzle(puzzleArr['value']);
-            setSolution(puzzleArr['solution']);
-            //setLoading(false);
-        }
+            setSolution(data.solution);
+            setRowhint(data.row_hints)
+            setColhint(data.column_hints)
+        }   
         catch (error) {
             console.error("Error fetching puzzle:", error)
         }
@@ -28,14 +28,14 @@ function NonoGame() {
       }, []);
 
       useEffect(() => {
-        if (puzzle !== null) {
-            console.log("Updated puzzle:", puzzle);
+        if (solution !== null) {
+            console.log("Solution:", solution);
         }
-    }, [puzzle]);
+    }, [solution]);
 
-    // return (
-    //     puzzle !== null && <SudGrid puzzle={puzzle} solution={solution}/>
-    // )
+    return (
+        solution !== null && <NonoGrid rowhint={rowhint} colhint={colhint} solution={solution}/>
+    )
 }
 
 export default NonoGame;
---
+++ front_end/src/components/NonoGrid.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import "../App.css";
+
+const SudGrid = ({ rowhint, colhint, solution }) => {
+    const [board, setBoard] = useState(solution);
+}
+
+export default NonoGrid;
